046e5a6de5e8e26b6f4c46d611c82cbdfaa7e706
Hitender kumar <hitender.hk@gmail.com>
2022-01-18 23:28:45 +0530

    'createTask' function to create as task and add to 'tasks' array & add uuid package for creating dynamic uuid

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid} from 'uuid'
 
 @Injectable()
 export class TasksService {
-    getAllTasks() { return []; }
+    private tasks: Task[] = []
+
+    getAllTasks() { return this.tasks }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title: title,
+            description: description,
+            status: TaskStatus.OPEN
+        }
+        this.tasks.push(task);
+        return task
+    }
 }
